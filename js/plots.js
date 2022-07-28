77b562007fac9fc3b99094c2108c457c24e14261
CorinneBean <ccookie281@gmail.com>
2022-07-28 11:54:57 -0500

    Initial commit

---
+++ js/plots.js
@@ -0,0 +1,26 @@
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
+
+init();
+
+function optionChanged(newSample) {
+  console.log(newSample);
+}
+
+function buildMetadata(Demographic_Info) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(results.location);
+  });
+}}
